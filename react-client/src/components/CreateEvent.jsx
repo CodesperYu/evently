f9d9d9a45acc266cbabb9aa374f4f6f7affa9e1b
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-06-28 11:35:21 -0700

    add fb.js, create event handler for all aspects of form

--- react-client/src/components/CreateEvent.jsx
+++ react-client/src/components/CreateEvent.jsx
@@ -10,17 +10,17 @@ class CreateEvent extends React.Component {
 			photoOptions: ['Wedding', 'Studio', 'Product', 'Landscape', 'Event'],
 			foodPicks: [],
 			musicPicks: [],
-			photographyPicks: [],
+			photoPicks: [],
 			foodBudget: 0,
 			musicBudget: 0,
-			photographyBudget: 0,
+			photoBudget: 0,
 			address: '',
 			notes: '',
 			defaultChecked: false
     }
   }
 
-	checkListsFood(event) {
+	checkListFood(event) {
 		var isChecked = event.target.checked;
 		var foodValue = event.target.value;
 		if (isChecked) {
@@ -53,20 +53,56 @@ class CreateEvent extends React.Component {
 		console.log(this.state.musicPicks);
 	}
 
-	checkListFood(event) {
+	checkListPhoto(event) {
 		var isChecked = event.target.checked;
-		var foodValue = event.target.value;
+		var photoValue = event.target.value;
 		if (isChecked) {
 			this.setState((prevState, props) => ({
-				foodPicks: prevState.foodPicks.concat([foodValue])
+				photoPicks: prevState.photoPicks.concat([photoValue])
 			}))
 		} else {
-			var foodIndex = this.state.foodPicks.indexOf(foodValue);
-			var newArray = this.state.foodPicks.splice(foodIndex,1);
+			var photoIndex = this.state.photoPicks.indexOf(photoValue);
+			var newArray = this.state.photoPicks.splice(photoIndex,1);
 			this.setState({
-				foodPicks: this.state.foodPicks
+				photoPicks: this.state.photoPicks
 			});
 		}
+		console.log(this.state.photoPicks);
+	}
+
+	foodBudgetChange(event) {
+		this.setState({
+			foodBudget: event.target.value
+		});
+		console.log(this.state.foodBudget);
+	}
+	
+	musicBudgetChange(event) {
+		this.setState({
+			musicBudget: event.target.value
+		});
+		console.log(this.state.musicBudget);
+	}
+
+	photoBudgetChange(event) {
+		this.setState({
+			photoBudget: event.target.value
+		});
+		console.log(this.state.photoBudget);
+	}
+
+	addressChange(event) {
+		this.setState({
+			address: event.target.value
+		});
+		console.log(this.state.address);
+	}
+
+	notesChange(event) {
+		this.setState({
+			notes: event.target.value
+		});
+		console.log(this.state.notes)
 	}
 
 	render () {
@@ -86,7 +122,7 @@ class CreateEvent extends React.Component {
 								</li>
 							))}
 							<li>
-								Budget: <input type="number" name="food budget" />
+								Budget: <input type="number" name="food budget" onChange={this.foodBudgetChange.bind(this)}/>
 							</li>
 						</ul>
 						<ul>
@@ -101,31 +137,33 @@ class CreateEvent extends React.Component {
 								</li>
 							))}
 							<li>
-								Budget: <input type="number" name="music budget" />
+								Budget: <input type="number" name="music budget" onChange={this.musicBudgetChange.bind(this)} />
 							</li>
 						</ul>
 						<ul>
 							<li>Photography</li>
 							{this.state.photoOptions.map((photo, key) => (
 								<li key={key}> 
-									<input type="checkbox" defaultChecked={this.state.defaultChecked} />
+									<input type="checkbox" defaultChecked={this.state.defaultChecked} 
+									value={photo}
+									onClick={this.checkListPhoto.bind(this)}/>
 									{photo}
 								</li>
 							))}
 							<li>
-								Budget: <input type="number" name="photography budget" />
+								Budget: <input type="number" name="photography budget" onChange={this.photoBudgetChange.bind(this)}/>
 							</li>
 						</ul>
 				</div>
 				<div id='location'>
 					<h1>Location</h1>
-					Address: <input type="text" name="address" />
+					Address: <input type="text" name="address" onChange={this.addressChange.bind(this)}/>
 				</div>
 				<div id='Note'>
 					<h1>Notes</h1>
 					Additional Information: 
 					<li>
-						<textarea rows="6" cols="60"></textarea>
+						<textarea rows="6" cols="60" onChange={this.notesChange.bind(this)}></textarea>
 					</li>
 				</div>
 			</div>
---
+++ server/config/fb.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'appID' : '2023460824555254',
+  'appSecret' : '5b6f862e9ca7cdc7bda32097254317a4',
+  'callbackUrl' : 'http://localhost:3000/auth/facebook/callbac
